7c7d5495bd23e9ee3ab71a921063ae9555c4d9e4
jonasa64 <hvidovrefifa@gmail.com>
2020-05-04 13:32:23 +0200

    updated drag and drop component

--- examenproject-angular/src/app/drag-drop/drag-drop.component.ts
+++ examenproject-angular/src/app/drag-drop/drag-drop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { moveItemInArray, transferArrayItem, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drag-drop',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragDropComponent implements OnInit {
 
+
+  itemsSolutions = [
+    'Save on your electricity and heat consumption',
+    'Planting of forest',
+    'Take the bike or public transport'
+  ];
+
+  itemConsequences = [
+    'Southern and Central Europe are seeing more heat waves, forest fires and droughts',
+    'Northern Europe is getting significantly wetter and it can become common with winter floods.',
+    'the concentration of CO2 in the atmosphere had risen steadily 285 ppm before industrialization in 1750 to 410 ppm today.'
+  ];
+
+  solutions = [];
+
+  consequences = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<String[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
   }
 
 }

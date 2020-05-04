428ff4c1de9a5fec2ec0693e47695a3be5da0490
jonasa64 <hvidovrefifa@gmail.com>
2020-05-04 12:00:42 +0200

    generated drag-drop component

--- examenproject-angular/src/app/app.module.ts
+++ examenproject-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropComponent
   ],
   imports: [
     BrowserModule,

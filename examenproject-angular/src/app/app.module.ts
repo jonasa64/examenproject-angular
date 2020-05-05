9ef5d72d10a02d31541f67fac38366b255e75b19
Henna <qureshi.henna@gmail.com>
2020-05-05 22:40:11 +0200

    Created component

--- examenproject-angular/src/app/app.module.ts
+++ examenproject-angular/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { MemoryGameComponent } from './memory-game/memory-game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DragDropComponent,
+    MemoryGameComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,

24fc79622e1c3d478bdfd7a35c8a960a771f9027
Henna <qureshi.henna@gmail.com>
2020-05-06 10:13:09 +0200

    Deleted memory game attempt

--- examenproject-angular/src/app/app.module.ts
+++ examenproject-angular/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import {NgxTimerModule} from 'ngx-timer';
+import { NgxTimerModule } from 'ngx-timer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DragDropComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,

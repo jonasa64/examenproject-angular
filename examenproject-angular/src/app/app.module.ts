2594ebf61098abcf9de36c69eec8b256b5008a4a
jonasa64 <hvidovrefifa@gmail.com>
2020-05-13 10:59:26 +0200

    Update app.module.ts

--- examenproject-angular/src/app/app.module.ts
+++ examenproject-angular/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgxTimerModule } from 'ngx-timer';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { MemoryComponent } from './memory/memory.component';
 import { CardComponent } from './memory/card/card.component';
+import { DragAndDropService } from '../app/services/drag-and-drop.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,10 @@ import { CardComponent } from './memory/card/card.component';
     BrowserAnimationsModule,
     DragDropModule,
     NgxTimerModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DragAndDropService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

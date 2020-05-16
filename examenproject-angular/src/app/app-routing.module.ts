789f83c9d75552f66ede39f0d2e358debc2877ef
Henna <qureshi.henna@gmail.com>
2020-05-17 01:25:47 +0200

    Front page styling mostly done

---
+++ examenproject-angular/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component'
+
+const routes: Routes = [
+  { path: '', component: FrontPageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

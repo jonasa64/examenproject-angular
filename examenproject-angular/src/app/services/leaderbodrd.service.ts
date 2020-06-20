3847344cc26176b4ccf654f7735b7baec08475ab
jonasa64 <hvidovrefifa@gmail.com>
2020-06-20 12:10:15 +0200

    Create leaderbodrd.service.ts

---
+++ examenproject-angular/src/app/services/leaderbodrd.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+}
+
+Injectable()
+export class LeaderbordService {
+    dragDropUrl = 'https://exam-backend-web.herokuapp.com/leaderbord/dragdrop'
+    quizUrl = 'https://exam-backend-web.herokuapp.com/leaderbord/quiz'
+
+    constructor(private http: HttpClient) { }
+
+    addDragDropLeaderdord(data) {
+        return this.http.post(this.dragDropUrl, data, httpOptions);
+    }
+
+    addQuizLeaderdord(data) {
+        return this.http.post(this.quizUrl, data, httpOptions);
+    }
+
+    getDragDropLeaderbord() {
+        return this.http.get(this.dragDropUrl);
+    }
+
+    getQuizLeaderbord() {
+        return this.http.get(this.quizUrl);
+    }
+
+}
